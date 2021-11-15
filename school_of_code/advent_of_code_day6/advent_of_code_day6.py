b2b33f17da5641a118e9889045103798826db07e
andreasp <andreas.pegelow@smarteye.se>
2021-11-15 11:10:22 +0100

    Advent of code day 6

---
+++ school_of_code/advent_of_code_day6/advent_of_code_day6.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+
+def main():
+    groups_answers = (
+        Path("school_of_code/advent_of_code_day6/input.txt").read_text().split("\n\n")
+    )
+
+    count = 0
+    for group_answers in groups_answers:
+        count += len(set(group_answers.replace("\n", "")))
+
+    print(f"Part 1: {count}")
+
+    count = 0
+    for group_answers in groups_answers:
+        answer_set = [set(answer) for answer in group_answers.split("\n")]
+        count += len(set.intersection(*answer_set))
+
+    print(f"Part 2: {count}")
+
+
+if __name__ == "__main__":
+    main()
