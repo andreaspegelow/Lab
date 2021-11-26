d4a999422fb4376238aa50e4d303d3b19fba805d
andreasp <andreas.pegelow@smarteye.se>
2021-11-26 17:32:12 +0100

    Advent of code day 8

---
+++ school_of_code/advent_of_code_day8/advent_of_code_day8.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from pathlib import Path
+import copy
+
+
+@dataclass
+class Instruction:
+    command: str
+    value: int
+    visited: bool = False
+
+
+def acc(PC, value, acc):
+    return PC + 1, acc + value
+
+
+def jmp(PC, value, acc):
+    return PC + value, acc
+
+
+def nop(PC, value, acc):
+    return PC + 1, acc
+
+
+class InfLoopException(Exception):
+    pass
+
+
+def parse_input(path):
+    instructions = []
+    input = Path(path).read_text().split("\n")
+    for line in input:
+        command, value = line.split(" ")
+        if command == "acc":
+            command = acc
+        if command == "jmp":
+            command = jmp
+        if command == "nop":
+            command = nop
+        instructions.append(Instruction(command, int(value)))
+    return instructions
+
+
+def interpeter(instructions):
+    accumulator = 0
+    pc = 0
+    while True:
+        try:
+            instruction = instructions[pc]
+        except IndexError:
+            return accumulator
+
+        if not instruction.visited:
+            pc, accumulator = instruction.command(pc, instruction.value, accumulator)
+            instruction.visited = True
+        else:
+            raise InfLoopException(f"InfLoopException, accumulator is: {accumulator}")
+
+
+def main():
+
+    # Part 1
+    instructions = parse_input("school_of_code/advent_of_code_day8/input.txt")
+    try:
+        interpeter(instructions)
+    except InfLoopException as e:
+        print(e)
+
+    # Part 2
+    instructions = parse_input("school_of_code/advent_of_code_day8/input.txt")
+    instructions_copy = instructions
+    for i, instruction in enumerate(instructions):
+
+        instructions_copy = copy.deepcopy(instructions)
+
+        if instruction.command == nop:
+            instructions_copy[i] = Instruction(jmp, instruction.value)
+        elif instruction.command == jmp:
+            instructions_copy[i] = Instruction(nop, instruction.value)
+        elif instruction.command == acc:
+            continue
+
+        try:
+            result = interpeter(instructions_copy)
+        except InfLoopException as e:
+            pass
+        else:
+            print(f"Found a correct solution by changing instruction in index: {i}! Accumulator is: {result}")
+            break
+
+
+if __name__ == "__main__":
+    main()
