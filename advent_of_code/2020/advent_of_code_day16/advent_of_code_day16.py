e3de12857fa1bda355e2464e5f3d51bc0d8fefd1
andreasp <andreas.pegelow@smarteye.se>
2021-12-03 19:21:27 +0100

    Create folders and files for all 2020

---
+++ advent_of_code/2020/advent_of_code_day16/advent_of_code_day16.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def parse_input(path: str):
+	return [int(i) for i in Path(path).read_text().split("\n")]
+
+
+def main():
+	input = parse_input("advent_of_code/2020/advent_of_code_day16/input.txt")
+
+
+if __name__ == "__main__":
+	main()
