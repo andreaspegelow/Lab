54fd425ded032638c54d1178b0da0d9b9dbe9a22
andreas.pegelow <andreas.pegelow@smarteye.se>
2022-09-09 10:00:42 +0200

    Regenerate templates for 2019

--- advent_of_code/2019/advent_of_code_day23/advent_of_code_day23.py
+++ advent_of_code/2019/advent_of_code_day23/advent_of_code_day23.py
@@ -3,12 +3,12 @@ from typing import List
 
 
 def parse_input(path: str) -> List[int]:
-	return [int(i) for i in Path(path).read_text().split("\n")]
+    return [int(i) for i in Path(path).read_text().split("\n")]
 
 
 def main():
-	input = parse_input("advent_of_code/2019/advent_of_code_day23/input.txt")
+    input = parse_input("advent_of_code/2019/advent_of_code_day23/input.txt")
 
 
 if __name__ == "__main__":
-	main()
+    main()
