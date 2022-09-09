7c743b017533e056b782b238b6ccdf0308f98f32
andreas.pegelow <andreas.pegelow@smarteye.se>
2022-09-09 09:57:06 +0200

    Advent of code day 2 2019

--- advent_of_code/2019/advent_of_code_day2/advent_of_code_day2.py
+++ advent_of_code/2019/advent_of_code_day2/advent_of_code_day2.py
@@ -1,14 +1,43 @@
+import itertools
+from copy import copy
+from operator import add, mul
 from pathlib import Path
 from typing import List
 
 
 def parse_input(path: str) -> List[int]:
-	return [int(i) for i in Path(path).read_text().split("\n")]
+    return [int(i) for i in Path(path).read_text().split(",")]
+
+
+def run_program(memory, noun, verb):
+    memory[1] = noun
+    memory[2] = verb
+
+    for pointer in range(0, len(memory), 4):
+        opcode = memory[pointer]
+        if opcode == 1:
+            op = add
+        elif opcode == 2:
+            op = mul
+        elif opcode == 99:
+            return memory[0]
+        else:
+            # Error!
+            return None
+
+        memory[memory[pointer + 3]] = op(
+            memory[memory[pointer + 1]], memory[memory[pointer + 2]]
+        )
 
 
 def main():
-	input = parse_input("advent_of_code/2019/advent_of_code_day2/input.txt")
+    input_memory = parse_input("advent_of_code/2019/advent_of_code_day2/input.txt")
+    for noun, verb in itertools.product(range(99), range(99)):
+        if (
+            output := run_program(copy(input_memory), noun, verb)
+        ) and output == 19690720:
+            print(f"Done! Result: {100 * noun + verb}")
 
 
 if __name__ == "__main__":
-	main()
+    main()
