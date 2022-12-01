eacb5d329bc2096552626c3a165a76ced655f0b2
andreas.pegelow <andreas.pegelow@smarteye.se>
2022-12-01 09:51:14 +0100

    Add templates for 2022

--- advent_of_code/create.py
+++ advent_of_code/create.py
@@ -1,9 +1,9 @@
 import os
 
-for i in range (3,26):
-    folder_path = f"advent_of_code/2019/advent_of_code_day{i}"
+for i in range (1,26):
+    folder_path = f"advent_of_code/2022/advent_of_code_day{i}"
     os.mkdir(folder_path)
     with open(f"{folder_path}/advent_of_code_day{i}.py", "w") as file:
-        file.write(f'from pathlib import Path\nfrom typing import List\n\n\ndef parse_input(path: str) -> List[int]:\n    return [int(i) for i in Path(path).read_text().split("\\n")]\n\n\ndef main():\n    input = parse_input("advent_of_code/2019/advent_of_code_day{i}/input.txt")\n\n\nif __name__ == "__main__":\n    main()\n')
+        file.write(f'from pathlib import Path\nfrom typing import List\n\n\ndef parse_input(path: str) -> List[int]:\n    return [int(i) for i in Path(path).read_text().split("\\n")]\n\n\ndef main():\n    puzzle_input = parse_input("advent_of_code/2022/advent_of_code_day{i}/input.txt")\n\n\nif __name__ == "__main__":\n    main()\n')
     with open(f"{folder_path}/input.txt", "w") as file:
         pass
