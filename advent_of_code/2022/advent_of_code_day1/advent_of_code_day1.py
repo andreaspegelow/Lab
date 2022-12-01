fc57d0fa7945abda13adcec86e2b3a255b724b87
andreas.pegelow <andreas.pegelow@smarteye.se>
2022-12-01 11:07:22 +0100

    Advent of code day 1 2022

--- advent_of_code/2022/advent_of_code_day1/advent_of_code_day1.py
+++ advent_of_code/2022/advent_of_code_day1/advent_of_code_day1.py
@@ -3,11 +3,17 @@ from typing import List
 
 
 def parse_input(path: str) -> List[int]:
-    return [int(i) for i in Path(path).read_text().split("\n")]
+    return Path(path).read_text().split("\n\n")
 
 
 def main():
-    puzzle_input = parse_input("advent_of_code/2022/advent_of_code_day1/input.txt")
+    elfs = parse_input("advent_of_code/2022/advent_of_code_day1/input.txt")
+
+    biggest_load = max(sum(map(int, elf.split("\n"))) for elf in elfs)
+    print(f"Part 1: {biggest_load}")
+
+    top3_total = sum(sorted(sum(map(int, elf.split("\n"))) for elf in elfs)[-3:])
+    print(f"Part 2: {top3_total}")
 
 
 if __name__ == "__main__":
