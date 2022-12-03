e3f47f2ac0480392312070af5d9836213eb59f9b
andreaspegelow <andpegelow@gmail.com>
2022-12-03 15:07:48 +0100

    Advent of code day 3 2022

--- advent_of_code/2022/advent_of_code_day3/advent_of_code_day3.py
+++ advent_of_code/2022/advent_of_code_day3/advent_of_code_day3.py
@@ -1,13 +1,33 @@
+import string
 from pathlib import Path
 from typing import List
 
 
 def parse_input(path: str) -> List[int]:
-    return [int(i) for i in Path(path).read_text().split("\n")]
+    return Path(path).read_text().split("\n")
 
 
 def main():
-    puzzle_input = parse_input("advent_of_code/2022/advent_of_code_day3/input.txt")
+    rugsacks = parse_input("advent_of_code/2022/advent_of_code_day3/input.txt")
+    alphabet = string.ascii_lowercase + string.ascii_uppercase
+
+    count = 0
+    for rugsack in rugsacks:
+        half = len(rugsack) // 2
+        common = {item for item in rugsack[:half] if item in rugsack[half:]}.pop()
+        count += alphabet.index(common) + 1
+    print(f"Part 1: {count}")
+
+    count = 0
+    for i, rugsack in enumerate(rugsacks[::3]):
+        i *= 3
+        badge = {
+            item
+            for item in rugsack
+            if item in rugsacks[i + 1] and item in rugsacks[i + 2]
+        }.pop()
+        count += alphabet.index(badge) + 1
+    print(f"Part 2: {count}")
 
 
 if __name__ == "__main__":
