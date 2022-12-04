4fa0e77aa811cce4875600df79f5c9330ff82ca7
andreaspegelow <andpegelow@gmail.com>
2022-12-04 20:50:09 +0100

    Advent of code day 4 2022

--- advent_of_code/2022/advent_of_code_day4/advent_of_code_day4.py
+++ advent_of_code/2022/advent_of_code_day4/advent_of_code_day4.py
@@ -3,11 +3,30 @@ from typing import List
 
 
 def parse_input(path: str) -> List[int]:
-    return [int(i) for i in Path(path).read_text().split("\n")]
+    p = []
+    for pairs in Path(path).read_text().split("\n"):
+        l = []
+        for section in pairs.split(","):
+            start, end = map(int, section.split("-"))
+            l.append(range(start, end))
+        l = sorted(l, key=lambda k: k.start)
+        p.append(l)
+    return p
 
 
 def main():
-    puzzle_input = parse_input("advent_of_code/2022/advent_of_code_day4/input.txt")
+    pairs = parse_input("advent_of_code/2022/advent_of_code_day4/input.txt")
+    selected = [
+        (first, second)
+        for first, second in pairs
+        if set(first).issubset(set(second)) or set(second).issubset(set(first))
+    ]
+    print(f"Part 1: {len(selected)}")
+
+    selected = [
+        (first, second) for first, second in pairs if first.stop >= second.start
+    ]
+    print(f"Part 2: {len(selected)}")
 
 
 if __name__ == "__main__":
